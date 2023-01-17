3e61459efd97a32ec9c534f34744ddb281872d1e
LiJunscs <119907438+LiJunscs@users.noreply.github.com>
2023-01-17 17:33:55 +0800

    version finish

--- MyClass.h
+++ MyClass.h
@@ -1,8 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<fstream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<vector>
+#include<string>
 #include<time.h>
 #include<Windows.h>
 #include<easyx.h>
@@ -12,56 +15,63 @@
 using namespace std;
 extern const int Win_width;
 extern const int Win_height;
-extern const char images[5][20];
-extern const char flowers[5][20];
+extern const char images[5][30];
+extern const char flowers[5][30];
 extern DWORD* pMem;
-class FireWork
+class Jet
 {
 public:
-	FireWork();
-	FireWork(int, int, int);
+	Jet();
+	Jet(int, int, int ,int);
 	void updata();
 	void draw();
-	void reset();
-	~FireWork();
+	void reset(int,int,int);
+	//int getX() {//获取当前烟花弹的x坐标
+	//	return this->x;
+	//}
+	//int getHy() {//获取当前烟花弹的最高位置y坐标
+	//	return this->hy;
+	//}
+	bool shoot_finish() {
+		return !this->isshoot;
+	}
+	~Jet();
 
 private:
 	IMAGE img;
 	int x, y, speed, hy;//当前位置，速度，最高位置
 	bool isshoot;
-	friend class Bomb;
 };
-FireWork::FireWork() {}
-FireWork::FireWork(int x, int hy, int img_index) {
-	this->y = Win_height;
-	this->x = x;
-	this->speed = -5;
-	this->hy = hy;
-	this->isshoot = true;
-	loadimage(&(this->img), images[img_index]);
+Jet::Jet() {}
+Jet::Jet(int x, int hy, int img_index, int speed =  -10) {
+	this->y = Win_height;//烟花弹初始位置设置为屏幕最下方
+	this->x = x;//初始x坐标设置为传入参数
+	this->speed = speed;//初始上升速度设置为-10
+	this->hy = hy;//初始最高位置y坐标设置为传入参数
+	this->isshoot = true;//初始发生状态设置为发射中
+	loadimage(&(this->img), images[img_index]);//加载图片
 }
-void FireWork::updata() {
-	if (this->isshoot)
+void Jet::updata() {
+	if (this->isshoot) {//如果烟花弹处于发生中，根据速度修改y坐标
 		this->y += this->speed;
-	if (this->y <= this->hy) {
+	}
+	if (this->y <= this->hy) {//如果烟花弹到达最高位置，发射状态修改为false
 		this->isshoot = false;
 	}
 }
-void FireWork::draw() {
-	if (this->isshoot) {
+void Jet::draw() {
+	if (this->isshoot) {//如果烟花弹处于发射中，贴图
 		putimage(this->x, this->y, &(this->img), SRCINVERT);
 	}
 }
-void FireWork::reset() {
-	if (this->isshoot == false) {
-		this->x = rand() % (Win_width - 20);
-		this->hy = Win_height / 2 - rand() % 200;
-		this->speed = -(rand() % 10 + 10);
-		this->y = Win_height;
-		this->isshoot = true;
-	}
+void Jet::reset(int x,int hy,int speed) {//发射并绽放完成后烟花弹进行重置，节省空间
+	this->x = x;//rand() % (Win_width - 20);//重置的烟花弹的起始坐标和速度都随机
+	this->hy = hy;//Win_height / 2 - rand() % 200;
+	this->speed = speed;//-(rand() % 10 + 10);
+	this->y = Win_height;//起始y坐标仍为屏幕最下方
+	this->isshoot = true;//设置发射状态为true
 }
-FireWork::~FireWork()
+Jet::~Jet()
 {
 }
 class Bomb
@@ -70,85 +80,131 @@ public:
 	Bomb();
 	Bomb(int, int, int);
 	void draw();
-	void reset();
+	void reset(int,int);
 	void updata();
-	bool iserase() {//用户点击生成的烟花的销毁函数
-		return this->jet.y == Win_height;
+	bool bomb_finish() {//烟花是否绽放完成
+		return this->isbomb;
+	}
+	void set_draw(bool isdraw) {//设置绽放状态
+		this->isdraw = isdraw;
 	}
 	~Bomb();
 
 private:
 	IMAGE img;
 	vector<vector<int>> pixels;
-	const int dtr[9] = { 5,5,7,7,12,12,20,20,27 };
+	int dtr[4] = { 5,5,7,7};
 	double t1, t2, dt;//用时间来控制爆炸散开速度
 	int center_x, center_y;//圆心坐标
 	int x, y;//相对于窗口的坐标;
 	int r, max_r;//爆炸半径和最大半径
-	bool isdraw;
-	FireWork jet;
+	bool isdraw, isbomb;//是否开始渲染，是否爆炸完成
 };
 Bomb::Bomb() {}
-Bomb::Bomb(int x, int y, int img_index) :jet(x, y, img_index) {
-	this->center_x = 90;
-	this->center_y = 90;
-	this->x = x;
+Bomb::Bomb(int x, int y, int img_index){
+	this->center_x = 40;//烟花绽放时的圆心坐标设置为图片的中心坐标
+	this->center_y = 40;
+	this->x = x;//烟花绽放时的相对于窗口的坐标设为传入的参数(即为烟花弹的起始x坐标和最高位置y坐标)
 	this->y = y;
-	this->t1 = GetTickCount64();
-	this->dt = 5;
-	this->r = 0;
-	this->max_r = 90;
-	this->isdraw = false;
-	loadimage(&(this->img), flowers[img_index]);
-	this->pixels.resize(180, vector<int>(180));
+	this->t1 = GetTickCount64();//速度由时间间隔控制
+	this->dt = 5;//初始时间间隔设置为5ms，随着绽放半径增大而增加
+	this->r = 0;//绽放的初始半径
+	this->max_r = 40;//绽放的最大半径
+	this->isdraw = false;//初始的渲染状态为false
+	this->isbomb = false;//初始的绽放状态为false
+	loadimage(&(this->img), flowers[img_index]);//加载图片
+	this->pixels.resize(80, vector<int>(80));//获取图片像素点
 	SetWorkingImage(&(this->img));
-	for (int i = 0; i < 180; i++) {
-		for (int j = 0; j < 180; j++) {
+	for (int i = 0; i < 80; i++) {
+		for (int j = 0; j < 80; j++) {
 			this->pixels[i][j] = getpixel(i, j);
 		}
 	}
 	SetWorkingImage(0);
 }
-void Bomb::updata() {
-	this->jet.updata();
-	if (this->jet.isshoot == false)
-		this->isdraw = true;
-}
-void Bomb::reset() {
-	this->t1 = GetTickCount64();
-	this->r = 0;
-	this->dt = 5;
-	this->x = this->jet.x;
-	this->y = this->jet.hy;
+void Bomb::reset(int x,int y) {
+	this->t1 = GetTickCount64();//重新获取时间间隔
+	this->r = 0;//恢复初始半径
+	this->dt = 5;//恢复初始时间间隔
+	this->x = x;//设置绽放时的相对于窗口的坐标
+	this->y = y;
+	this->isbomb = false;//设置绽放状态为false
+	this->isdraw = false;//设置绽放状态为false
 }
 void Bomb::draw() {
-	this->jet.draw();
-	this->t2 = GetTickCount64();
-	if (this->t2 - this->t1 > this->dt && this->isdraw) {
-		if (this->r < this->max_r) {
+	this->t2 = GetTickCount64();//获取当前时间
+	if (this->t2 - this->t1 > this->dt && this->isdraw) {//判断时间间隔大小和是否绽放完成
+		if (this->r < this->max_r) {//绽放半径和速度随绽放时间增长
 			this->r++;
 			this->dt = this->dtr[this->r / 10];
+			//this->isdraw = true;
 		}
 		if (this->r >= this->max_r - 1) {
 			this->isdraw = false;
-			this->jet.reset();
-			this->reset();
+			this->isbomb = true;
 		}
 		this->t1 = this->t2;
 	}
-	if (this->isdraw) {
+	if (this->isdraw) {//如果允许渲染，则根据圆的参数方程计算绽放中心周围像素点的真实坐标，和相对于图片中心的坐标
 		for (double i = 0.0; i < 6.28; i += 0.01) {
-			double degree_x = this->r * cos(i);
+			double degree_x = this->r * cos(i);//像素点的角度值
 			double degree_y = this->r * sin(i);
-			int x1 = (int)(this->center_x + degree_x);
+			int x1 = (int)(this->center_x + degree_x);//像素点相对于图片中心的坐标
 			int y1 = (int)(this->center_y + degree_y);
-			int xx = this->x + degree_x;
+			int xx = this->x + degree_x;//像素点相对于窗口的坐标
 			int yy = this->y + degree_y;
-			if (x1 >= 0 && x1 < 180 && y1 >= 0 && y1 < 180 && xx >= 0 && xx <= Win_width && yy >= 0 && yy <= Win_height)
+			//判断越界,并输出到显存
+			if (x1 >= 0 && x1 < 80 && y1 >= 0 && y1 < 80 && xx >= 0 && xx <= Win_width && yy >= 0 && yy <= Win_height)
 				pMem[yy * Win_width + xx] = BGR(this->pixels[x1][y1]);
 		}
 	}
 }
 Bomb::~Bomb()
 {
+}
+class FireWork
+{
+public:
+	FireWork();
+	FireWork(int, int, int, bool);
+	void updata();
+	void draw();
+	void reset(int,int,int);
+	bool isFinish() {
+		return this->bomb.bomb_finish();
+	}
+	~FireWork();
+
+private:
+	Jet jet;//将烟花拆分成烟花弹和爆炸绽放两部分
+	Bomb bomb;
+	bool iserase;//用来标记是否是鼠标点击生成的烟花，如果是则会在绽放结束后被析构，如果是初始时的烟花则不会
+};
+
+FireWork::FireWork():iserase(false){}
+FireWork::FireWork(int x, int hy, int img_index,bool erase = false) :jet(x, hy, img_index), bomb(x, hy, img_index), iserase(erase) {
+}
+void FireWork::updata() {
+	this->jet.updata();//先更新烟花弹的位置
+	if (this->jet.shoot_finish()) {//如果烟花弹发射结束，则烟花图案绽放{
+		this->bomb.set_draw(true);
+	}
+
+}
+//在窗口中画烟花弹和烟花绽放
+void FireWork::draw() {
+	this->jet.draw();
+	this->bomb.draw();
+}
+//初始的烟花绽放完后重置属性，继续使用
+void FireWork::reset(int x,int hy,int speed) {
+	//如果不是鼠标点击生成的烟花且已经绽放完成则重置
+	if (this->iserase == false&&this->bomb.bomb_finish()) {
+		this->jet.reset(x,hy,speed);
+		this->bomb.reset(x,hy);
+	}
+}
+FireWork::~FireWork()
+{
+	cout << "烟花类的析构函数被调用" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
 #include"MyClass.h"
+const char images[5][30] = { "resource\\shoot0.png","resource\\shoot1.png","resource\\shoot2.png","resource\\shoot3.png","resource\\shoot4.png" };
+const char flowers[5][30] = { "resource\\flower0.png","resource\\flower1.png" ,"resource\\flower2.png" ,"resource\\flower3.png" ,"resource\\flower4.png" };
+const int heart_cooradinates[12][2] = { {485,110},{717,110},{372,180},{830,180},{389,280},{813,280},{446,360},{756,360},{522,420},{680,420},{601,200},{601,460} };
+const char happy_new_year[30] = "resource\\happy_new_year.png";
+const int heart_speed[6] = { -25,-21,-16,-12,-9,-7 };
 const int Win_width = 1200;
 const int Win_height = 600;
-const char images[5][20] = { "shoot0.png","shoot1.png","shoot2.png","shoot3.png","shoot4.png" };
-const char flowers[5][20] = { "flower0.png","flower1.png" ,"flower2.png" ,"flower3.png" ,"flower4.png" };
-vector<Bomb> fire_works;
-vector<Bomb> clicks;
+vector<FireWork> fire_works;
+vector<FireWork> clicks;
+vector<FireWork> heart;
+IMAGE new_year_img;
+char text[6][80];
+int textSize;
+bool heart_flag;
 DWORD* pMem;
-bool MouseClick;
 void Init();
-void draw();
 void clearImage();
+void draw();
 void updata();
+void reset();
 void userClick();
+void printText();
+void count_down();
+void blessing_text();
+void setTextStyle();
 int main() {
 	Init();
+	mciSendString("play resource\\bk_music.mp3", 0, 0, 0);
+	blessing_text();
+	count_down();
+	mciSendString("play resource\\firework.mp3 repeat", 0, 0, 0);
 	BeginBatchDraw();
 	while (1) {
-		Sleep(16);
+		Sleep(25);
 		clearImage();
-		draw();
+		putimage(120, Win_height - 100, &new_year_img);
 		userClick();
+		draw();
 		updata();
+		printText();
 		draw();
 		FlushBatchDraw();
+		reset();
 	}
 	getchar();
 	closegraph();
@@ -31,39 +50,89 @@ int main() {
 void Init() {
 	srand(time(NULL));
 	initgraph(Win_width, Win_height);
-	pMem = GetImageBuffer();
-	for (int i = 0; i < 5; i++) {
+	pMem = GetImageBuffer();//获取显存指针
+	heart_flag = false;
+	mciSendString("open resource\\bk_music.mp3", 0, 0, 0);
+	mciSendString("open resource\\firework.mp3", 0, 0, 0);//打开烟花背景音乐并循环播放
+	loadimage(&new_year_img, happy_new_year);
+	setTextStyle();//设置字体和颜色
+	setbkmode(TRANSPARENT);
+	settextcolor(YELLOW);
+	for (int i = 0; i < 4; i++) {//初始化三个烟花
 		int x = rand() % (Win_width - 40) + i * 10;
 		int hy = Win_height / 2 - rand() % 200;
-		Bomb tmp(x, hy, i);
+		FireWork tmp(x, hy, i);
 		fire_works.push_back(tmp);
 	}
+	for (int i = 0; i < 12; i++) {
+		FireWork tmp(heart_cooradinates[i][0], heart_cooradinates[i][1], (i / 2) % 4);
+		heart.push_back(tmp);
+	}
+	ifstream fp;
+	fp.open("resource\\user_defined.txt");
+	while (textSize < 6 && fp >> text[textSize])
+		textSize++;
+	fp.close();
 }
 void updata() {
 	for (auto p = fire_works.begin(); p != fire_works.end(); p++)
 		p->updata();
-	int i = 0;
-	while (clicks.empty() == false && i < clicks.size()) {
-		if (clicks[i].iserase())
-			clicks.erase(clicks.begin() + i);
+	for (auto p = clicks.begin(); p != clicks.end();) {
+		if (p->isFinish())
+			p = clicks.erase(p);
 		else {
-			clicks[i].updata();
-			i++;
+			p->updata();
+			p++;
 		}
 	}
+	if (heart_flag) {
+		for (auto p = heart.begin(); p != heart.end(); p++)
+			p->updata();
+		if (heart[10].isFinish())
+			heart_flag = false;
+	}
+}
+void reset() {
+	static auto it = fire_works.begin();
+	if (it != fire_works.end()) {
+		int x = 0;
+		if (heart_flag) {
+			x = rand() % 360;
+			if (x & 1)
+				x += 480;
+		}
+		else x= rand() % (Win_width - 20);
+		int hy= Win_height / 2 - rand() % 250;
+		int speed= -(rand() % 10 + 10);
+		it->reset(x,hy,speed);
+		it++;
+	}
+	else it = fire_works.begin();
+	if (heart_flag == false) {
+		for (auto p = heart.begin(); p != heart.end(); p++) {
+			int index = p - heart.begin();
+			int v = 0;
+			if (index < 10)
+				v = heart_speed[index / 2];
+			else v = heart_speed[1];
+			p->reset(heart_cooradinates[index][0], heart_cooradinates[index][1], v);
+		}
+	}
+	//Sleep(5);
 }
 void draw() {
-	for (auto p = fire_works.begin(); p != fire_works.end(); p++) {
+	for (auto p = fire_works.begin(); p != fire_works.end(); p++)
 		p->draw();
-	}
 	for (auto p = clicks.begin(); p != clicks.end(); p++)
+		p->draw();
+	for (auto p = heart.begin(); p != heart.end(); p++)
 		p->draw();
 }
 void clearImage() {
 	for (int i = 0; i < 2000; i++)
 	{
 		int px1 = rand() % Win_width; // 0  to 1199
-		int py1 = rand() % Win_height;  // 0 to 799
+		int py1 = rand() % (Win_height - 100);  // 0 to 599
 
 		pMem[py1 * Win_width + px1] = BLACK;//将像素点赋值为背景色即为擦除
 		pMem[py1 * Win_width + px1 + 1] = BLACK;	// 对显存赋值擦出像素点		
@@ -72,12 +141,60 @@ void clearImage() {
 void userClick() {
 	ExMessage msg;
 	if (peekmessage(&msg)) {
-		if (msg.message == WM_LBUTTONDOWN)
-			MouseClick = true;
-		if (MouseClick) {
-			int index = rand() % 5;
-			Bomb tmp(msg.x, Win_height - 44, index);
+		if (msg.message == WM_LBUTTONDOWN) {
+			int index = rand() % 4;
+			FireWork tmp(msg.x, msg.y, index, true);
 			clicks.push_back(tmp);
 		}
+		else if (msg.message == WM_RBUTTONDOWN) {
+			heart_flag = true;
+			clearImage();
+		}
 	}
+}
+void printText() {
+	static int Time = 0;
+	const int y = Win_height - 100 -textSize * 40;
+	Time++;
+	outtextxy(450, y + Time / 90 * 40, text[Time / 90]);
+	if (Time == 450)
+		Time = 0;
+}
+void count_down() {
+	char count_img[5][30] = { "resource\\count_1.png","resource\\count_2.png","resource\\count_3.png","resource\\count_4.png","resource\\count_5.png" };
+	for (int i = 0; i < 5; i++) {
+		IMAGE img;
+		loadimage(&img, count_img[4 - i]);
+		putimage(80, 0, &img);
+		Sleep(1000);
+	}
+	cleardevice();
+}
+void blessing_text() {
+	char blessing[6][100] = { "\0" };
+	int blessingSize = 0, x, y;
+	ifstream fp;
+	fp.open("resource\\user_defined_blessing.txt");
+	while (blessingSize < 6 && fp >> blessing[blessingSize])
+		blessingSize++;
+	fp.close();
+	x = 450, y = Win_height - 300 - blessingSize * 40;
+	for (int i = 0; i < blessingSize; i++) {
+		outtextxy(x, y + i * 40, blessing[i]);
+		Sleep(1000);
+	}
+	cleardevice();
+	outtextxy(400, 250, "来一场绚丽的烟火表演吧!");
+	outtextxy(400, 300, "准备好了吗?");
+	Sleep(1500);
+	cleardevice();
+}
+void setTextStyle() {
+	LOGFONT f;
+	gettextstyle(&f);
+	f.lfHeight = 30;
+	f.lfWeight = 15;
+	_tcscpy_s(f.lfFaceName, "微软雅黑");
+	f.lfQuality = ANTIALIASED_QUALITY;
+	settextstyle(&f);
 }
